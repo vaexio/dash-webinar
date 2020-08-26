ca336c650611b99480191779c3cd010e6e308b2a
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-08-26 11:28:13 +0200

    since /app is home, we need to link it

--- launch.sh
+++ launch.sh
@@ -7,10 +7,17 @@ NAME=app
 # We skip all cells tagged 'skip-app' to speed up startup time
 jupyter nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-app',)"  --to script $NAME.ipynb
 
-# Make sure the files exists (otherwise all workers try to create it at the same time)
-python ./prefetch.py
+
 # To avoid having to fetch the data each time, make sure the /root/.vaex/file-cache directory
 # is persistent (setup Directory Mappings in Dash Enterprise)
+# e.g. map /data/vaex-file-cache/ to /root/.vaex
+# But inside dokku, $HOME points to /app (which Dash Enterprise cannot map)
+# The solution is to link these directories
+ln -s /app/.vaex /root/.vaex
+
+
+# Make sure the files exists (otherwise all workers try to create it at the same time)
+python ./prefetch.py
 
 # Depending on notebook metadata, command above may output a .txt file
 # If so, change extension to .py
